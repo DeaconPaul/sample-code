ffd0559b6923ed286ecc04331bc001f3da9b0a63
DeaconPaul <rp_crafts@att.net>
2015-10-24 07:13:34 -0500

    added comment and done message

--- csv 1.py
+++ csv 1.py
@@ -7,3 +7,5 @@ with open('input Jeremy sector forecasts 2015_10_2.csv', 'rb') as f:
     reader = csv.reader(f)
     for row in reader:
         print row
+#------------------------------------------------------------
+print "done"
